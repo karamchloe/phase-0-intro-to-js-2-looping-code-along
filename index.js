f147adca5f5182b99e32b70e7f54f9bb5540e7a1
Chloe Park <kpark0119@gmail.com>
2023-09-13 19:19:53 -0400

    for-looping and arraying

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards([Guadalupe, Ollie, Aki]){ 
+    const message = [
+    "Thank you, Guadalupe, for the wonderful surprise gift!",
+    "Thank you, Ollie, for the wonderful surprise gift!",
+    "Thank you, Aki, for the wonderful surprise gift!"
+]
+return message;
+
+
+}
+
+function countDown(x){
+    for(let i=x; i >=0; i--){
+        console.log(i)
+    }
+    
+}
+
+
+
+console.log(countDown(10));
